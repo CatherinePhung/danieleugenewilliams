a77f2b5facbecfaaea2e4ab02e178c86264d4a97
DanielEugeneWilliams <DanielEugeneWilliams@gmail.com>
2012-10-12 19:32:02 -0400

    added check for [n] isdigit

--- CS50/pset5_forensics/bmp/resize.c
+++ CS50/pset5_forensics/bmp/resize.c
@@ -9,6 +9,8 @@
        
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "bmp.h"
 
@@ -23,6 +25,17 @@ main(int argc, char *argv[])
         return 1;
     }
     
+    // check that resize factor is digit
+    for(int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if(!isdigit(argv[1][i]))
+        {
+            printf("Usage: resize [n] infile.bmp outfile.bmp\n");
+            printf("Usage: [n] must be an integer\n");
+            return 1;
+        }
+    }
+    
     // resize multiple
     int resize_multiple = atoi(argv[1]);
     
@@ -30,7 +43,7 @@ main(int argc, char *argv[])
     {
         printf("Usage: resize [n] infile.bmp outfile.bmp\n");
         printf("Usage: [n] must be between 1 and 100\n");
-        return 1;
+        return 2;
     }
 
     // remember filenames
@@ -42,7 +55,7 @@ main(int argc, char *argv[])
     if (inptr == NULL)
     {
         printf("Could not open %s.\n", infile);
-        return 2;
+        return 3;
     }
 
     // open output file
@@ -51,7 +64,7 @@ main(int argc, char *argv[])
     {
         fclose(inptr);
         fprintf(stderr, "Could not create %s.\n", outfile);
-        return 3;
+        return 4;
     }
 
     // read infile's BITMAPFILEHEADER
@@ -69,7 +82,7 @@ main(int argc, char *argv[])
         fclose(outptr);
         fclose(inptr);
         fprintf(stderr, "Unsupported file format.\n");
-        return 4;
+        return 5;
     }
     
     // START UPDATE HEADER INFO
